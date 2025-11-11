ff8d9ca63a50a37df5589caafe3ad1aa93b80852
Diron <dironkim@mail.ru>
2025-11-11 09:09:35 +0300

    added ts dependencies + log

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { Bot } from '@maxhub/max-bot-api';
 import 'dotenv/config';
 const bot = new Bot(process.env.BOT_TOKEN);
+console.log("executing...");
 // Обработчик события запуска бота
 bot.on('bot_started', (ctx) => ctx.reply('Привет! Я - официальный чат-бот Вятского агротехнологического университета.'));
 // Обработчик для всех входящих сообщений
 bot.on('message_created', (ctx) => ctx.reply("Я еще нахожусь в разработке и не могу ответить на Ваш вопрос."));
 bot.start();
+console.log("bot started");
 //# sourceMappingURL=index.js.map
